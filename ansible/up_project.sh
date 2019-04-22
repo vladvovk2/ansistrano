acd4769a1a84014434efb1fcb30bf3e9bbf5319b
vladvovk2 <vlad.vovk.844@gmail.com>
2019-04-22 21:03:24 +0300

    sdfsdf

--- ansible/up_project.sh
+++ ansible/up_project.sh
@@ -1,8 +1,8 @@
-docker-compose -f docker-compose.production.yml down
-docker-compose -f docker-compose.production.yml build
-docker-compose -f docker-compose.production.yml run -rm rails rake db:create
-docker-compose -f docker-compose.production.yml run -rm rails rake db:migrate
-docker-compose -f docker-compose.production.yml stop
-docker-compose -f docker-compose.production.yml up -d
+docker-compose -f docker-compose-production.yml down
+docker-compose -f docker-compose-production.yml build
+docker-compose -f docker-compose-production.yml run -rm rails rake db:create
+docker-compose -f docker-compose-production.yml run -rm rails rake db:migrate
+docker-compose -f docker-compose-production.yml stop
+docker-compose -f docker-compose-production.yml up -d
 sleep 5
 docker ps
